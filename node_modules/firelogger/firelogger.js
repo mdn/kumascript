27e0baca8dec19862dac3c823028b8acaacd2276
Les Orchard <me@lmorchard.com>
2012-03-12 21:08:19 -0400

    Accept `X-FireLogger: plaintext` header to skip Base64 encoding JSON messages

--- node_modules/firelogger/firelogger.js
+++ node_modules/firelogger/firelogger.js
@@ -36,11 +36,21 @@ module.exports = function (options) {
             var fl_ver = req.header('X-FireLogger');
             if (!fl_ver) { return wh_next(); }
 
-            var d_logs = { logs: messages },
-                d_json = JSON.stringify(d_logs),
-                d_b64 = (new Buffer(d_json, 'utf-8')).toString('base64'),
-                d_lines = d_b64.match(/(.{1,75})/g),
-                uid = parseInt(Math.random() * 1000000, 16);
+            var uid = parseInt(Math.random() * 1000000, 16);
+            var d_lines = [];
+            if ('plaintext' == fl_ver) {
+                // Non-standard `X-FireLogger: plaintext` header skips the
+                // base64 part and sticks each JSON-encoded log message into a
+                // header. Good for debugging by curl
+                d_lines = messages.map(JSON.stringify);
+            } else {
+                // `X-FireLogger: 1.2` is what's expected from the add-on, but
+                // accept anything else.
+                var d_logs = { logs: messages },
+                    d_json = JSON.stringify(d_logs),
+                    d_b64 = (new Buffer(d_json, 'utf-8')).toString('base64');
+                d_lines = d_b64.match(/(.{1,75})/g);
+            }
 
             for (var i=0; i<d_lines.length; i++) {
                 res.header('FireLogger-' + uid + '-' + i, d_lines[i]);
