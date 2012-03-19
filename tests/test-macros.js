ab6d3daa3b3d60b9f356e3409001cef914e35701
Les Orchard <me@lmorchard.com>
2012-03-19 12:54:31 -0400

    bug 730715: Bugfix where kumascript never responds for a document with no macros

--- tests/test-macros.js
+++ tests/test-macros.js
@@ -154,6 +154,25 @@ module.exports = nodeunit.testCase({
                 test.done();
             }
         );
+    },
+
+    "Documents with no macros should not cause the server to hang": function (test) {
+        var done = false;
+        var mp = new ks_macros.MacroProcessor({ 
+            loader_class: ks_test_utils.JSONifyLoader
+        });
+        setTimeout(function () {
+            if (done) { return; }
+            test.ok(false, 'Test timed out, assuming a hang.');
+            test.done();
+        }, 250);
+        processFixture(test, mp, 'macros-no-macros.txt',
+            function (errors, result) {
+                test.ok(!errors, "There should be no errors");
+                test.done();
+                done = true;
+            }
+        );
     }
 
 });
