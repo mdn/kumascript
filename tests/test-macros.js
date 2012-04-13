9c00b3131ea3194d8cad425dcbb3c4032e04bf4d
Les Orchard <me@lmorchard.com>
2012-04-13 13:57:56 -0400

    bug 730707: Show context from doc source in errors
    * For both DocumentParsingError and TemplateExecutionError, locate the
      error in the document source and include some lines from the source
      indicating where the error happened.
    
    * Bugfix for }} in documents, which should be valid plain text when not
      preceeded by {{

--- tests/test-macros.js
+++ tests/test-macros.js
@@ -48,12 +48,28 @@ module.exports = nodeunit.testCase({
             function (errors, result) {
 
                 test.ok(errors, "There should be errors");
-                test.ok(errors.length, 1, "There should be 1 error");
+                test.equal(errors.length, 1, "There should be 1 error");
 
                 var e = errors[0];
                 test.equal(e.name, 'DocumentParsingError');
                 test.equal(0, e.message.indexOf('Syntax error at line'));
+
+                // Note: This is a *bit* brittle, but it makes sure the error
+                // indicator appears at the expected spot in the context lines
+                // included in the message.
+                test.equal(265, e.message.indexOf('-----------------------------^'));
                 
+                test.done();
+            });
+    },
+
+    "Double right brace in a document should not result in a syntax error": function (test) {
+        var mp = new ks_macros.MacroProcessor({ 
+            loader_class: ks_test_utils.JSONifyLoader
+        });
+        processFixture(test, mp, 'macros-document-double-brace.txt',
+            function (errors, result) {
+                test.ok(!errors, "There should be no errors");
                 test.done();
             });
     },
@@ -110,6 +126,11 @@ module.exports = nodeunit.testCase({
                     test.equal(errors[idx].name, expected_errors[idx][0]);
                     test.ok(errors[idx].message.indexOf(expected_errors[idx][1]) !== -1);
                 }
+
+                // Note: This is a *bit* brittle, but it makes sure the error
+                // indicator appears at the expected spot in the context lines
+                // included in the message.
+                test.equal(295, errors[2].message.indexOf('---------------------^'));
 
                 test.done();
             }
