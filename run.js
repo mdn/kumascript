38ad969e030f11f30acdaa9582e9f7b732eec3b8
Les Orchard <me@lmorchard.com>
2012-07-13 14:55:19 -0400

    Rework server runner, add a telnet REPL interface for localhost debugging and restarts

--- run.js
+++ run.js
@@ -7,6 +7,8 @@
 // ### Prerequisites
 var util = require('util'),
     fs = require('fs'),
+    net = require('net'),
+    repl = require('repl'),
     _ = require('underscore'),
     
     nconf = require('nconf'),
@@ -35,6 +37,11 @@ var DEFAULT_CONFIG = {
             "https://developer.mozilla.org/en-US/docs/{path}?raw=1",
         template_url_template:
             "https://developer.mozilla.org/en-US/docs/en-US/Template:{name}?raw=1"
+    },
+    repl: {
+        enabled: true,
+        host: "127.0.0.1",
+        port: 9070
     }
 };
 
@@ -79,52 +86,129 @@ if (log_conf.file) {
 // TODO: Accept log line format from config?
 // TODO: Use [winston-syslog](https://github.com/indexzero/winston-syslog)?
 
+// Make a nicer alias to the default logger
+var log = winston;
+
 // ### Initialize a server
-var server_conf = nconf.get('server'),
-    server = new ks_server.Server(server_conf);
+var server_conf = nconf.get('server');
 
-// ### Fire up the server, or hand off to manager.
-if (require.main === module) {
-    var cluster = require('cluster');
+function startWorker() {
+    log.info("Worker PID " + process.pid + " starting");
+    process.on('uncaughtException', function (err) {
+        log.error('uncaughtException:', err.message);
+        log.error(err.stack);
+        process.exit(1);
+    });
+    var server = new ks_server.Server(server_conf);
+    server.listen();
+}
 
-    if (cluster.isMaster) {
-        var num_workers = server_conf.numWorkers || 
-                          require('os').cpus().length;
-        var exiting = false;
-        var workers = {};
+function startMaster() {
+    log.info("Master PID " + process.pid + " starting");
 
-        for (var i = 0; i < num_workers; i++) {
-            var worker = cluster.fork();
-            workers[worker.pid] = worker;
-        }
+    var num_workers = server_conf.numWorkers || 
+                      require('os').cpus().length;
+    var exiting = false;
+    var workers = {};
 
-        cluster.on('death', function(worker) {
-            // Make sure not to keep restarting workers while exiting
-            if (exiting) { return; }
-            console.log('worker ' + worker.pid + ' died');
-            delete workers[worker.pid];
-            var new_worker = cluster.fork();
-            workers[new_worker.pid] = new_worker;
-        });
-
-        // See: https://github.com/joyent/node/issues/2060#issuecomment-2767191
-        process.on('SIGINT', function () {
-            exiting = true;
-            console.log("master exiting");
-            for (pid in workers) {
-                var worker = workers[pid];
-                console.log("kill worker " + worker.pid);
-                worker.kill();
-            };
-            process.exit(1);
-        });
-        
-    } else {
-        process.on('uncaughtException', function (err) {
-            console.error('uncaughtException:', err.message);
-            console.error(err.stack);
-            process.exit(1);
-        });
-        server.listen();
+    for (var i = 0; i < num_workers; i++) {
+        var worker = cluster.fork();
+        workers[worker.pid] = worker;
     }
+
+    cluster.on('death', function(worker) {
+        // Make sure not to keep restarting workers while exiting
+        if (exiting) { return; }
+        log.info('Worker ' + worker.pid + ' died');
+        delete workers[worker.pid];
+        var new_worker = cluster.fork();
+        workers[new_worker.pid] = new_worker;
+    });
+
+    // Kill the master and all workers. If this is being monitored at the
+    // OS level, the whole thing should restart.
+    function performExit () {
+        exiting = true;
+        log.info("Master exiting...");
+        for (pid in workers) {
+            var worker = workers[pid];
+            worker.kill();
+            log.info("Killed worker " + worker.pid);
+        };
+        process.exit(1);
+    }
+
+    // See: https://github.com/joyent/node/issues/2060#issuecomment-2767191
+    process.on('SIGINT', performExit);
+
+    // Open up a telnet REPL for interactive access to the server.
+    var repl_config = nconf.get('repl');
+    if (repl_config.enabled) {
+        // TODO: Move this off into its own repl.js module?
+
+        // Things to expose to the REPL
+        var context = {
+            __: _,
+            log: log,
+            server_conf: server_conf,
+            workers: workers,
+
+            // Force-exit the master, which hopefully gets restarted by an
+            // OS-level monitor like supervisor.
+            kill: performExit,
+
+            // Kill all the workers and let the master restart them. More
+            // forgiving than kill()
+            restart: function () {
+                log.info("Recycling workers...");
+                var killed = [];
+                for (pid in workers) {
+                    var worker = workers[pid];
+                    worker.kill();
+                    log.info("Killed worker " + worker.pid);
+                    killed.push(pid);
+                };
+                return killed;
+            }
+        };
+
+        // REPL eval handler that logs all commands
+        // Cribbed from https://github.com/joyent/node/blob/v0.6/lib/repl.js#L76
+        var vm = require('vm');
+        var eval = function(code, context, file, cb) {
+            log.info("REPL (cmd): > " + util.inspect(code));
+            var err, result;
+            try {
+                result = vm.runInContext(code, context, file);
+            } catch (e) {
+                err = e;
+            }
+            log.info("REPL (result): " + util.inspect([err, result]));
+            cb(err, result);
+        };
+
+        // Finally, set up the server to accept REPL connections.
+        var host = repl_config.host;
+        var port = repl_config.port;
+        net.createServer(function (socket) {
+            var r_host = socket.remoteAddress;
+            var r_port = socket.remotePort;
+            var shell = repl.start("ks> ", socket, eval);
+            _(shell.context).extend(context);
+            log.info("REPL received connection from "+r_host+":"+r_port); 
+            socket.on('close', function () {
+                log.info("REPL connection closed for "+r_host+":"+r_port);
+            });
+        }).listen(port, host)
+        log.info("Telnet REPL interface started on " + host + ":" + port);
+    }
+
+}
+
+// Fire up the master or the worker, as appropriate.
+var cluster = require('cluster');
+if (!cluster.isMaster) {
+    startWorker();
+} else {
+    startMaster();
 }
