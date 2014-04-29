16a20f011e1656620b43198652fcc9f80a5d8ff4
Les Orchard <me@lmorchard.com>
2014-04-28 20:46:10 -0400

    Update memcached dependency

--- node_modules/memcached/node_modules/hashring/node_modules/bisection/tests/bisection.test.js
+++ node_modules/memcached/node_modules/hashring/node_modules/bisection/tests/bisection.test.js
@@ -1,5 +1,5 @@
 var should = require('should')
-  , bisection = require('../lib/bisection');
+  , bisection = require('..');
 
 var small = [0,1,2,3,5,6,7,8,9]
   , large = [122,124,134,176,320,341,345,346,500,587,666,768,1230,23231];
@@ -16,7 +16,7 @@ module.exports = {
     bisection(small, 15).should.equal(9);
     bisection(small, 4, 5).should.equal(5);
     bisection(small, 12, 0, 5).should.equal(5);
-    
+
     bisection(large, 350).should.equal(8);
     bisection(large, 242).should.equal(4);
     bisection(large, 0).should.equal(0);
@@ -27,7 +27,7 @@ module.exports = {
     bisection.left(small, 15).should.equal(0);
     bisection.left(small, 4, 5).should.equal(9);
     bisection.left(small, 12, 0, 5).should.equal(0);
-    
+
     bisection.left(large, 350).should.equal(0);
     bisection.left(large, 242).should.equal(14);
     bisection.left(large, 0).should.equal(14);
