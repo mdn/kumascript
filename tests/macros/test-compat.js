a303dd22e09acfac2188f5bcdad88c74f306eb3f
Florian Scholz <elchi3@elchi3.de>
2017-10-19 22:31:33 +0200

    jsdom approach

--- tests/macros/test-compat.js
+++ tests/macros/test-compat.js
@@ -6,41 +6,30 @@ const fs = require('fs'),
       utils = require('./utils'),
       chai = require('chai'),
       chaiAsPromised = require('chai-as-promised'),
+      jsdom = require("jsdom"),
       assert = chai.assert,
       itMacro = utils.itMacro,
       describeMacro = utils.describeMacro,
       beforeEachMacro = utils.beforeEachMacro,
       fixture_dir = path.resolve(__dirname, 'fixtures/compat');
 
+const { JSDOM } = jsdom;
+
 // Let's add "eventually" to assert so we can work with promises.
 chai.use(chaiAsPromised);
 
-// Load fixture data.
-const fixtures = {
-    alternative_name: {
-        data: null,
-        dataFile: 'alternative_name.json',
-        expected: '',
-        expectedFile: 'alternative_name-expected.txt',
-    }
-};
-
-for (const name in fixtures) {
-    fixtures[name].data = JSON.parse(
-        fs.readFileSync(path.resolve(fixture_dir, fixtures[name].dataFile), 'utf8')
-    );
-
-    fixtures[name].expected =
-        fs.readFileSync(path.resolve(fixture_dir, fixtures[name].expectedFile), 'utf8');
-}
+let fixtureCompatData = {};
+fs.readdirSync(fixture_dir).forEach(function(fn) {
+    Object.assign(fixtureCompatData, JSON.parse(
+        fs.readFileSync(path.resolve(fixture_dir, fn), 'utf8')
+    ));
+});
 
 describeMacro('Compat', function () {
 
     beforeEachMacro(function (macro) {
         macro.ctx.require = sinon.stub();
-        macro.ctx.require.withArgs('mdn-browser-compat-data').returns(
-        fixtures.alternative_name.data
-   );
+        macro.ctx.require.withArgs('mdn-browser-compat-data').returns(fixtureCompatData);
     });
 
     itMacro('Outputs a message if there is no data for the query "foo.bar"', function (macro) {
@@ -49,11 +38,23 @@ describeMacro('Compat', function () {
         return assert.eventually.equal(actual, expected);
     });
 
-    itMacro('Alternative names are written as footnotes', function (macro) {
-        // do the magic to load the alternative_name stub instead of the real data
-        let actual = macro.call('webextensions.api.menus.ContextType');
-        let expected = fixtures.alternative_name.expected;
-        return assert.eventually.equal(actual, expected);
+    itMacro('Alternative names are rendered as footnotes', function (macro) {
+        let actual = macro.call('alternative_name.feature');
+        return actual.then(function(result) {
+            let dom = JSDOM.fragment(result);
+
+            // Alternative name in simple cell
+            assert.equal(dom.querySelectorAll("tbody td")[1].outerHTML,
+            '<td class="full-support">10<sup><a href="#compatNote_1">1</a></sup></td>');
+            assert.equal(dom.querySelector("#compatNote_1").outerHTML,
+            '<p id="compatNote_1">1. Supported as <code>contextMenus.ContextType</code>.</p>');
+
+            // Alternative name in cell with multipe support ranges
+            assert.equal(dom.querySelectorAll("tbody td")[3].outerHTML,
+            '<td class="full-support"><p>12</p><p>5<sup><a href="#compatNote_2">2</a></sup></p></td>');
+            assert.equal(dom.querySelector("#compatNote_2").outerHTML,
+            '<p id="compatNote_2">2. Supported as <code>CookieMonsterInterface</code>.</p>');
+        });
     });
 
 });
