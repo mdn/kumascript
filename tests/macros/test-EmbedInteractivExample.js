41c1e35045d5ecaf5bd446384af409cf4ac620d3
John Whitlock <jwhitlock@mozilla.com>
2018-08-16 17:27:10 -0500

    EmbedInteractivExample: refactor adding classes
    Remove the extra spaces when extra classes are not added, and add test
    cases for adding classes.

--- tests/macros/test-EmbedInteractivExample.js
+++ tests/macros/test-EmbedInteractivExample.js
@@ -17,7 +17,7 @@ describeMacro('EmbedInteractiveExample', function () {
         };
         return assert.eventually.equal(
             macro.call('pages/css/animation.html'),
-            `<iframe class="interactive  " width="100%" height="250" frameborder="0" src="https://interactive-examples.mdn.mozilla.net/pages/css/animation.html" title="MDN Web Docs Interactive Example"></iframe>`
+            `<iframe class="interactive" width="100%" height="250" frameborder="0" src="https://interactive-examples.mdn.mozilla.net/pages/css/animation.html" title="MDN Web Docs Interactive Example"></iframe>`
         );
     });
     itMacro('Changes in settings and argument are reflected', function (macro) {
@@ -26,7 +26,7 @@ describeMacro('EmbedInteractiveExample', function () {
         };
         return assert.eventually.equal(
             macro.call('pages/http/headers.html'),
-            `<iframe class="interactive  " width="100%" height="250" frameborder="0" src="https://www.fleetwood-mac.com/pages/http/headers.html" title="MDN Web Docs Interactive Example"></iframe>`
+            `<iframe class="interactive" width="100%" height="250" frameborder="0" src="https://www.fleetwood-mac.com/pages/http/headers.html" title="MDN Web Docs Interactive Example"></iframe>`
         );
     });
     itMacro('Trailing slash in setting and leading slash in argument', function (macro) {
@@ -35,7 +35,7 @@ describeMacro('EmbedInteractiveExample', function () {
         };
         return assert.eventually.equal(
             macro.call('/pages/css/animation.html'),
-            `<iframe class="interactive  " width="100%" height="250" frameborder="0" src="https://interactive-examples.mdn.mozilla.net/pages/css/animation.html" title="MDN Web Docs Interactive Example"></iframe>`
+            `<iframe class="interactive" width="100%" height="250" frameborder="0" src="https://interactive-examples.mdn.mozilla.net/pages/css/animation.html" title="MDN Web Docs Interactive Example"></iframe>`
         );
     });
     itMacro('origin query param is added when not in production', function (macro) {
@@ -45,7 +45,7 @@ describeMacro('EmbedInteractiveExample', function () {
         macro.ctx.env.url = 'https://developer.allizom.org/en-US/docs/Web/HTTP/headers';
         return assert.eventually.equal(
             macro.call('pages/http/headers.html'),
-            `<iframe class="interactive  " width="100%" height="250" frameborder="0" src="https://interactive-examples.mdn.mozilla.net/pages/http/headers.html?origin=https://developer.allizom.org" title="MDN Web Docs Interactive Example"></iframe>`
+            `<iframe class="interactive" width="100%" height="250" frameborder="0" src="https://interactive-examples.mdn.mozilla.net/pages/http/headers.html?origin=https://developer.allizom.org" title="MDN Web Docs Interactive Example"></iframe>`
         );
     });
     itMacro('origin query param is added to existing query', function (macro) {
@@ -55,7 +55,34 @@ describeMacro('EmbedInteractiveExample', function () {
         macro.ctx.env.url = 'http://localhost:8000/en-US/docs/Web/HTTP/headers';
         return assert.eventually.equal(
             macro.call('pages/http/headers.html?foo=bar'),
-            `<iframe class="interactive  " width="100%" height="250" frameborder="0" src="https://interactive-examples.mdn.mozilla.net/pages/http/headers.html?foo=bar&origin=http://localhost:8000" title="MDN Web Docs Interactive Example"></iframe>`
+            `<iframe class="interactive" width="100%" height="250" frameborder="0" src="https://interactive-examples.mdn.mozilla.net/pages/http/headers.html?foo=bar&origin=http://localhost:8000" title="MDN Web Docs Interactive Example"></iframe>`
+        );
+    });
+    itMacro('Javascript pages get an extra class by default', function (macro) {
+        macro.ctx.env.interactive_examples = {
+            base_url: "https://interactive-examples.mdn.mozilla.net",
+        };
+        return assert.eventually.equal(
+            macro.call('pages/js/expressions-conditionaloperators.html'),
+            `<iframe class="interactive interactive-js" width="100%" height="250" frameborder="0" src="https://interactive-examples.mdn.mozilla.net/pages/js/expressions-conditionaloperators.html" title="MDN Web Docs Interactive Example"></iframe>`
+        );
+    });
+    itMacro('An extra class can be passed as an argument', function (macro) {
+        macro.ctx.env.interactive_examples = {
+            base_url: "https://interactive-examples.mdn.mozilla.net",
+        };
+        return assert.eventually.equal(
+            macro.call('pages/http/headers.html', 'extra'),
+            `<iframe class="interactive extra" width="100%" height="250" frameborder="0" src="https://interactive-examples.mdn.mozilla.net/pages/http/headers.html" title="MDN Web Docs Interactive Example"></iframe>`
+        );
+    });
+    itMacro('Javascript pages can also add an extra class', function (macro) {
+        macro.ctx.env.interactive_examples = {
+            base_url: "https://interactive-examples.mdn.mozilla.net",
+        };
+        return assert.eventually.equal(
+            macro.call('pages/js/expressions-conditionaloperators.html', 'bigger'),
+            `<iframe class="interactive interactive-js bigger" width="100%" height="250" frameborder="0" src="https://interactive-examples.mdn.mozilla.net/pages/js/expressions-conditionaloperators.html" title="MDN Web Docs Interactive Example"></iframe>`
         );
     });
 });
