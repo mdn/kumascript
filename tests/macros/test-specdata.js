b64115936c7e8db3ca45b6fe2ef26d38be56d9ee
Will Bamberg <wbamberg@mozilla.com>
2018-10-26 20:49:38 -0700

    Fix test code

--- tests/macros/test-specdata.js
+++ tests/macros/test-specdata.js
@@ -2,7 +2,6 @@
 
 const utils = require('./utils'),
       chai = require('chai'),
-      jsdom = require('jsdom'),
       assert = chai.assert,
       itMacro = utils.itMacro,
       describeMacro = utils.describeMacro;
@@ -23,15 +22,22 @@ const specStatusValues = [
     'LC'
 ];
 
+/*
+Performs basic validation of the SpecData JSON object:
+ * All entries must have name, url, and status properties.
+ * The url property must be an HTTPS URL.
+ * The status property must be in the list of valid status values.
+ */
 function checkSpecData(specDataJson) {
-    const entries = Object.entries(specDataJson);
-    for (let entry of entries) {
-        assert(entry.name !== undefined, 'SpecData entry is missing required "name" property');
-        assert(entry.url !== undefined, 'SpecData entry is missing required "url" property');
-        assert(entry.status !== undefined, 'SpecData entry is missing required "status" property');
+    const entries = Object.entries(JSON.parse(specDataJson));
 
-        assert(entry.url.startsWith('https://'), 'SpecData "url" entry is not an HTTPS URL');
-        assert(specStatusValues.includes(entry.status), 'SpecData "status" entry is not a valid value');
+    for (let entry of entries) {
+        assert(entry[1].name !== undefined, `SpecData entry: ${entry[0]} is missing required "name" property`);
+        assert(entry[1].url !== undefined, `SpecData entry: ${entry[0]} is missing required "url" property`);
+        assert(entry[1].status !== undefined, `SpecData entry: ${entry[0]} is missing required "status" property`);
+
+        assert(entry[1].url.startsWith('https://'), `SpecData entry: ${entry[0]}: "url" is not an HTTPS URL`);
+        assert(specStatusValues.includes(entry[1].status), `SpecData: ${entry[0]}: "status" is not a valid value`);
     }
 }
 
