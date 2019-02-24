c77099f780855acb27fe5a01a3a160edec20f373
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2019-02-24 16:30:00 +0100

    docs: Add JSDoc for `tests/macros/utils.js`

--- tests/macros/utils.js
+++ tests/macros/utils.js
@@ -11,55 +11,185 @@ const vnu = require('vnu-jar');
 const Environment = require('../../src/environment.js');
 const Templates = require('../../src/templates.js');
 
-// When we were doing mocha testing, we used this.macro to hold this.
-// But Jest doesn't use the this object, so we just store the object here.
+// TODO: Maybe use a class for this?
+/**
+ * @typedef Macro
+ * @property {typeof Environment.prototype.prototypeEnvironment} ctx
+ *           Give the test-case writer access to the macro's globals (ctx).
+ *           For example, "macro.ctx.env.locale" can be manipulated to something
+ *           other than 'en-US' or "macro.ctx.wiki.getPage" can be mocked
+ *           using `jest.fn()` to avoid network calls.
+ *
+ * @property {(name: string, result: string) => void} mockTemplate
+ *           When writing tests for a macro that invokes other macros with
+ *           the `template()` function, you sometimes want to specify
+ *           a mock return value for those other macros.
+ *
+ *           This function provides a much easier way to handle that than
+ *           using `jest.fn()` directly.
+ *
+ *           To unmock a template result, simply call `unmockTemplate()`
+ *           with the same `name`.
+ *
+ * @property {(name: string) => boolean} unmockTemplate
+ *           Stops mocking the result of a `template()` function call.
+ *
+ * @property {(...args: any[]) => Promise<string>} call
+ *           Use this function to make test calls on the named macro, if applicable.
+ *           Its arguments become the arguments to the macro. It returns a promise.
+ */
+
+/**
+ * When we were doing mocha testing, we used this.macro to hold this.
+ * But Jest doesn't use the this object, so we just store the object here.
+ * @type {Macro}
+ */
 let macro = null;
 
+/**
+ * Asserts that value is truthy.
+ *
+ * @template T Type of value.
+ * @param {T} x Actual value.
+ */
 function assert(x) {
-    expect(x).toBe(true);
+    expect(x).toBeTruthy();
 }
 
+/**
+ * Asserts non-strict equality (==) of actual and expected.
+ *
+ * @template T Type of the objects.
+ * @param {T} x Actual value.
+ * @param {any} y Potential expected value.
+ */
 assert.equal = (x, y) => {
     expect(x).toEqual(y);
 };
 
 assert.eventually = {
+    /**
+     * Asserts non-strict equality (==) of actual and expected.
+     *
+     * @template T Type of the objects.
+     * @param {T|PromiseLike<T>} x Actual value.
+     * @param {any} y Potential expected value.
+     */
     async equal(x, y) {
         expect(await x).toEqual(y);
     }
 };
 
-assert.include = (list, element) => {
-    expect(list).toContain(element);
-};
+/**
+ * Asserts that value is true.
+ *
+ * @template T Type of value.
+ * @param {T} value Actual value.
+ */
 assert.isTrue = value => {
     expect(value).toEqual(true);
 };
+
+/**
+ * Asserts that value is false.
+ *
+ * @template T Type of value.
+ * @param {T} value Actual value.
+ */
 assert.isFalse = value => {
     expect(value).toEqual(false);
 };
+
+/**
+ * Asserts value is strictly greater than (>) floor.
+ *
+ * @param {number} value Actual value.
+ * @param {number} floor Minimum Potential expected value.
+ */
 assert.isAbove = (value, floor) => {
     expect(value).toBeGreaterThan(floor);
 };
+
+/**
+ * Asserts that value is an array.
+ *
+ * @template T Type of value.
+ * @param {T} value Actual value.
+ */
 assert.isArray = value => {
     expect(value).toBeInstanceOf(Array);
 };
+
+/**
+ * Asserts that value is an object of type 'Object'
+ * (as revealed by Object.prototype.toString).
+ *
+ * @template T Type of value.
+ * @param {T} value Actual value.
+ * @remarks The assertion does match subclassed objects.
+ */
 assert.isObject = value => {
     expect(value).toBeInstanceOf(Object);
 };
+
+/**
+ * Asserts that value is a function.
+ *
+ * @template T Type of value.
+ * @param {T} value Actual value.
+ */
 assert.isFunction = value => {
     expect(value).toBeInstanceOf(Function);
 };
+
+/**
+ * Asserts that object has a property named by property.
+ *
+ * @template T Type of object.
+ * @param {T} value Container object.
+ * @param {string} prop Potential contained property of object.
+ */
 assert.property = (value, prop) => {
     expect(value).toHaveProperty(prop);
 };
+
+/**
+ * Asserts that object doesn't have a property named by property.
+ *
+ * @template T Type of object.
+ * @param {T} value Container object.
+ * @param {string} prop Potential contained property of object.
+ */
 assert.notProperty = (value, prop) => {
     expect(value).not.toHaveProperty(prop);
 };
+
+/**
+ * Asserts that set1 and set2 have the same members. Order is not take into account.
+ *
+ * @template T Type of set values.
+ * @param {T[]} a1 Actual set of values.
+ * @param {T[]} a2 Potential expected set of values.
+ */
 assert.sameMembers = (a1, a2) => {
     expect(new Set(a1)).toEqual(new Set(a2));
 };
 
+/**
+ * Asserts that haystack includes needle.
+ *
+ * @template T
+ * @param {string|T[]} list Container string or array.
+ * @param {string|T} element Potential value contained in haystack.
+ */
+assert.include = (list, element) => {
+    expect(list).toContain(element);
+};
+
+/**
+ * @param {string} macroName
+ * @return {Macro}
+ */
 function createMacroTestObject(macroName) {
     let templates = new Templates(__dirname + '/../../macros/');
     let pageContext = {
@@ -165,7 +295,8 @@ function afterEachMacro(teardown) {
  * success, and, on failure, a string detailing all of the errors.
  *
  * @param {string} html
- * @param {boolean} fragment
+ * @param {boolean} [fragment]
+ * @return {string|null}
  */
 function lintHTML(html, fragment = true) {
     if (fragment) {
@@ -183,7 +314,7 @@ function lintHTML(html, fragment = true) {
         });
         return null;
     } catch (error) {
-        const error_message = error.message
+        const error_message = /** @type {Error} */ (error).message
             .split(os.EOL)
             .filter(line => line.startsWith('Error: '))
             .join(os.EOL);
