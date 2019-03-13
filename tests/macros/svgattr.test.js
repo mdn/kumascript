afc23b9de3f4a319b8bc30b74f161e25b7b27e18
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2019-03-13 08:46:10 -0700

    Update tests/macros/svgattr.test.js
    Co-Authored-By: escattone <escattone@gmail.com>

--- tests/macros/svgattr.test.js
+++ tests/macros/svgattr.test.js
@@ -6,7 +6,7 @@ const { assert, itMacro, describeMacro } = require('./utils');
 describeMacro('SVGAttr', () => {
     for (const locale of ['en-US', 'de', 'fr']) {
         for (const attr of ['min', 'max']) {
-            itMacro(`${locale} ${attr} `, function(macro) {
+            itMacro(`${locale} ${attr} `, macro => {
                 macro.ctx.env.locale = locale;
                 return assert.eventually.equal(
                     macro.call(attr),
