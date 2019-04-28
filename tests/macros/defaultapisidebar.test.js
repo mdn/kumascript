aa1a8274f1176958ac18ce1be567c17f8c486af1
Will Bamberg <wbamberg@mozilla.com>
2019-04-28 12:23:54 -0700

    Add tests for DefaultAPISidebar

---
+++ tests/macros/defaultapisidebar.test.js
@@ -0,0 +1,359 @@
+/**
+ * @prettier
+ */
+const { JSDOM } = require('jsdom');
+
+const {
+    beforeEachMacro,
+    describeMacro,
+    itMacro,
+    lintHTML
+} = require('./utils');
+
+/**
+* Load all the fixtures.
+*/
+const fs = require('fs');
+const path = require('path');
+const subpagesFixturePath = path.resolve(__dirname, 'fixtures/defaultapisidebar/subpages.json');
+const subpagesFixture = JSON.parse(fs.readFileSync(subpagesFixturePath, 'utf8'));
+const commonl10nFixturePath = path.resolve(__dirname, 'fixtures/defaultapisidebar/commonl10n.json');
+const commonl10nFixture = fs.readFileSync(commonl10nFixturePath, 'utf8');
+const commonL10nJSON = JSON.parse(commonl10nFixture);
+const groupDataFixturePath = path.resolve(__dirname, 'fixtures/defaultapisidebar/groupdata.json');
+const groupDataFixture = fs.readFileSync(groupDataFixturePath, 'utf8');
+
+/**
+* All the const objects that follow define bits of the data we expect.
+**/
+
+/**
+* Map link names to link targets
+* (excluding locales, which the checker adds as a prefix)
+*/
+const expectedTargets = {
+    // overview page
+    'TestInterface1 API': '/docs/Web/API/TestInterface1_API',
+    'TestInterface2 API': '/docs/Web/API/TestInterface2_API',
+    'TestInterface3 API': '/docs/Web/API/TestInterface3_API',
+    // guides
+    'A Guide in another place': '/docs/Web/AnotherPlace/A_guide',
+    'A Guide which is a subpage': 'docs/Web/API/TestInterface_API/MyGuidePage2',
+    'A Guide which is also a subpage': '/docs/Web/API/TestInterface_API/MyGuidePage1',
+    // interfaces
+    'AnInterface': '/docs/Web/API/AnInterface',
+    'AnotherInterface': '/docs/Web/API/AnotherInterface',
+    // methods
+    'AnInterface.doSomething()': '/docs/Web/API/AnInterface/doSomething',
+    'ADifferentInterface.doSomething()': '/docs/Web/API/ADifferentInterface/doSomething',
+    'YetAnotherInterface.doSomething()': '/docs/Web/API/YetAnotherInterface/doSomething',
+    // properties
+    'ADifferentInterface.aProperty': '/docs/Web/API/ADifferentInterface/aProperty',
+    'ADifferentInterface.anotherProperty': '/docs/Web/API/ADifferentInterface/anotherProperty',
+    // events
+    'anEvent': '/docs/Web/Events/anEvent',
+    'anotherEvent': '/docs/Web/Events/anotherEvent'
+}
+
+/**
+* Data for guide links is more complex because (when the guides are
+* found from subpages rather than GroupData) it is
+* locale-sensitive and contains title attributes.
+*/
+const expectedGuides = {
+    'en-US': {
+        'A Guide in another place': {
+            text: 'A Guide in another place',
+            target: '/en-US/docs/Web/AnotherPlace/A_guide'
+        },
+        'A Guide listed in GroupData and also a subpage_1': {
+            text: 'A Guide listed in GroupData and also a subpage',
+            target: '/en-US/docs/Web/API/TestInterface_API/MyGuidePage1'
+        },
+        'A Guide listed in GroupData and also a subpage_2': {
+            text: 'A Guide listed in GroupData and also a subpage',
+            target: '/en-US/docs/Web/API/TestInterface_API/MyGuidePage1',
+            title: 'The MyGuidePage1 ...'
+        },
+        'A Guide which is only a subpage': {
+            text: 'A Guide which is only a subpage',
+            target: '/en-US/docs/Web/API/TestInterface_API/MyGuidePage2',
+            title: 'The MyGuidePage2 ...'
+        }
+    },
+    'fr':  {
+        'A Guide in another place': {
+            text: 'A Guide in another place',
+            target: '/fr/docs/Web/AnotherPlace/A_guide'
+        },
+        'A Guide listed in GroupData and also a subpage_1': {
+            text: 'A Guide listed in GroupData and also a subpage',
+            target: '/fr/docs/Web/API/TestInterface_API/MyGuidePage1'
+        },
+        'A Guide listed in GroupData and also a subpage_2': {
+            text: 'A Guide listed in GroupData and also a subpage [Traduire]',
+            target: '/fr/docs/Web/API/TestInterface_API/MyGuidePage1',
+            title: 'The MyGuidePage1 ...'
+        },
+        'A Guide which is only a subpage': {
+            text: 'A Guide which is only a subpage [Traduire]',
+            target: '/fr/docs/Web/API/TestInterface_API/MyGuidePage2',
+            title: 'The MyGuidePage2 ...'
+        }
+    },
+    'ja':  {
+        'A Guide in another place': {
+            text: 'A Guide in another place',
+            target: '/ja/docs/Web/AnotherPlace/A_guide'
+        },
+        'A Guide listed in GroupData and also a subpage_1': {
+            text: 'A Guide listed in GroupData and also a subpage',
+            target: '/ja/docs/Web/API/TestInterface_API/MyGuidePage1'
+        },
+        'A Guide listed in GroupData and also a subpage_2': {
+            text: 'A Guide listed in GroupData and also a subpage (ja translation)',
+            target: '/ja/docs/Web/API/TestInterface_API/MyGuidePage1',
+            title: 'The MyGuidePage1 ... (ja translation).'
+        },
+        'A Guide which is only a subpage': {
+            text: 'A Guide which is only a subpage (ja translation)',
+            target: '/ja/docs/Web/API/TestInterface_API/MyGuidePage2',
+            title: 'The MyGuidePage2 ... (ja translation).'
+        }
+    }
+}
+
+/**
+* Different expected sidebar contents, one for each test
+*/
+const expectedSideBarContents = {
+    'TestInterface1_WithSubpages': {
+        'overview': 'TestInterface1 API',
+        details: {
+            Guides: ['A Guide listed in GroupData and also a subpage_2', 'A Guide which is only a subpage'],
+            Interfaces: ['AnInterface'],
+            Methods: ['ADifferentInterface.doSomething()', 'YetAnotherInterface.doSomething()']
+        }
+    },
+    'TestInterface2_WithSubpages': {
+        'overview':   'TestInterface2 API',
+        details: {
+            Guides: ['A Guide listed in GroupData and also a subpage_2', 'A Guide which is only a subpage', 'A Guide in another place'],
+            Interfaces: ['AnInterface'],
+            Methods: ['ADifferentInterface.doSomething()', 'YetAnotherInterface.doSomething()'],
+            Properties: ['ADifferentInterface.aProperty', 'ADifferentInterface.anotherProperty'],
+            Events: ['anEvent']
+        }
+    },
+    'TestInterface3_WithSubpages': {
+        'overview': 'TestInterface3 API',
+        details: {
+            Guides: ['A Guide listed in GroupData and also a subpage_2', 'A Guide which is only a subpage', 'A Guide in another place', 'A Guide listed in GroupData and also a subpage_1'],
+            Interfaces: ['AnInterface', 'AnotherInterface'],
+            Properties: ['ADifferentInterface.aProperty']
+        }
+    },
+    'TestInterface4_WithSubpages': {
+        details: {
+            Guides: ['A Guide in another place'],
+            Interfaces: ['AnInterface', 'AnotherInterface'],
+            Properties: ['ADifferentInterface.aProperty'],
+            Events: ['anEvent', 'anotherEvent']
+        }
+    },
+    'TestInterface1_NoSubpages': {
+        'overview':   'TestInterface1 API',
+        details: {
+            Interfaces: ['AnInterface'],
+            Methods: ['ADifferentInterface.doSomething()', 'YetAnotherInterface.doSomething()']
+        }
+    },
+    'TestInterface2_NoSubpages': {
+        'overview':   'TestInterface2 API',
+        details: {
+            Guides: ['A Guide in another place'],
+            Interfaces: ['AnInterface'],
+            Methods: ['ADifferentInterface.doSomething()', 'YetAnotherInterface.doSomething()'],
+            Properties: ['ADifferentInterface.aProperty', 'ADifferentInterface.anotherProperty'],
+            Events: ['anEvent']
+       }
+    }
+}
+
+/**
+* This is the checker for all items except for guide items.
+* It just compares:
+* - the DOM element's textContent with expected,
+* - the `a.href` with the expected target, which it gets from
+* the `expectedTargets` map.
+*/
+function checkItem(expected, actual, locale) {
+    // Check the textContent
+    expect(actual.textContent).toEqual(expected);
+    // Check the target
+    const itemLink = actual.querySelector('a');
+    const expectedTarget = expectedTargets[expected];
+    expect(itemLink.href).toEqual(`/${locale}${expectedTarget}`);
+}
+
+/**
+* Guide items need special treatment because they
+* are locale-sensitive sometimes, and sometimes include a title
+* attribute which we have to check.
+*/
+function checkGuideItem(expected, actual, locale) {
+    // Check the textContent
+    const expectedItem = expectedGuides[locale][expected];
+    expect(actual.textContent).toEqual(expectedItem.text);
+    // Check the target
+    const itemLink = actual.querySelector('a');
+    expect(itemLink.href).toEqual(expectedItem.target);
+    // Check the title
+    if (expectedItem.title) {
+        expect(itemLink.getAttribute('title')).toEqual(expectedItem.title);
+    }
+}
+
+/**
+* Check a subsection of the sidebar, represented as a <details> element
+* containing a <summary> and a list of links.
+*/
+function checkSubList(name, config, details, checker, next) {
+    const hasSubList = config.expected.details[name];
+    if (hasSubList) {
+        // Check the summary
+        const expectedSummary = commonL10nJSON[name][config.locale];
+        const actual = details[next];
+        const actualSummary = actual.querySelector('summary');
+        expect(actualSummary.textContent).toEqual(expectedSummary);
+        // Check the list
+        const actualItems = actual.querySelectorAll('ol>li');
+        const expectedItems = config.expected.details[name];
+        expect(actualItems.length).toEqual(expectedItems.length);
+        for (let i = 0; i < actualItems.length; i++) {
+            checker(expectedItems[i], actualItems[i], config.locale);
+        }
+        next++;
+    }
+    return next;
+}
+
+/**
+* This is the entry point for checking the result of a test.
+* config.expected contains the expected results, and we use other bits
+* of config, most notably locale.
+*/
+function checkResult(html, config) {
+    // Lint the HTML
+    expect(lintHTML(html)).toBeFalsy();
+
+    const dom = JSDOM.fragment(html);
+    // Check that all links reference the proper locale or use https
+    const num_total_links = dom.querySelectorAll('a[href]').length;
+    const num_valid_links = dom.querySelectorAll(`a[href^="/${config.locale}/"], a[href^="https://"]`).length;
+    expect(num_valid_links).toEqual(num_total_links);
+
+    if (config.expected.overview) {
+        // Test overview link
+        const overviewLink = dom.querySelector('ol>li>strong');
+        checkItem(config.expected.overview, overviewLink, config.locale);
+    }
+
+    // Test sublists
+    const details = dom.querySelectorAll('ol>li>details');
+    expect(details.length).toEqual(Object.keys(config.expected.details).length);
+    // "_" prefix is to suppress lint's no-unused-vars
+    let _next = 0;
+    _next = checkSubList('Guides', config, details, checkGuideItem, _next);
+    _next = checkSubList('Interfaces', config, details, checkItem, _next);
+    _next = checkSubList('Properties', config, details, checkItem, _next);
+    _next = checkSubList('Methods', config, details, checkItem, _next);
+    _next = checkSubList('Events', config, details, checkItem, _next);
+}
+
+/**
+* Call the macro for each of three locales,
+* using the given config, and check the result.
+*/
+function testMacro(config) {
+    for (const locale of ['en-US', 'fr', 'ja']) {
+        let testName = `${config.name}; locale: ${locale}`;
+        itMacro(testName, function(macro) {
+            config.locale = locale;
+            macro.ctx.env.locale = locale;
+            // Mock calls to MDN.subpagesExpand
+            macro.ctx.page.subpagesExpand = jest.fn((page) => {
+                return config.subpages;
+            });
+            return macro.call(config.argument).then(function(result) {
+                checkResult(result, config);
+            });
+
+        });
+    }
+}
+
+describeMacro('DefaultAPISidebar', function() {
+
+    // Set any fixtures that don't change from one
+    // test to another
+    beforeEachMacro(function(macro) {
+        // env.slug only has to be truthy
+        macro.ctx.env.slug = 'not undefined';
+        // Mock calls to L10n-Common and GroupData
+        const originalTemplate = macro.ctx.template;
+        macro.ctx.template = jest.fn( async (name, ...args) => {
+            if (name === "L10n:Common") {
+                return commonl10nFixture;
+            }
+            if (name === "GroupData") {
+                return groupDataFixture;
+            }
+            return await originalTemplate(name, ...args);
+        });
+    });
+
+    // Run each test case with its own config
+    testMacro({
+        name: 'Text Interface 1 with subpages',
+        argument: 'TestInterface1',
+        subpages: subpagesFixture,
+        expected: expectedSideBarContents.TestInterface1_WithSubpages
+    });
+
+    testMacro({
+        name: 'Text Interface 2 with subpages',
+        argument: 'TestInterface2',
+        subpages: subpagesFixture,
+        expected: expectedSideBarContents.TestInterface2_WithSubpages
+    });
+
+    testMacro({
+        name: 'Text Interface 3 with subpages',
+        argument: 'TestInterface3',
+        subpages: subpagesFixture,
+        expected: expectedSideBarContents.TestInterface3_WithSubpages
+    });
+
+    testMacro({
+        name: 'Text Interface 4 with subpages',
+        argument: 'TestInterface4',
+        subpages: subpagesFixture,
+        expected: expectedSideBarContents.TestInterface4_WithSubpages
+    });
+
+    testMacro({
+        name: 'Text Interface 1 with no subpages',
+        argument: 'TestInterface1',
+        subpages: [],
+        expected: expectedSideBarContents.TestInterface1_NoSubpages
+    });
+
+    testMacro({
+        name: 'Text Interface 2 with no subpages',
+        argument: 'TestInterface2',
+        subpages: [],
+        expected: expectedSideBarContents.TestInterface2_NoSubpages
+    });
+
+});
