a43950e89326dc4ad705b551947d1209b2a10230
Rick Waldron <waldron.rick@gmail.com>
2019-10-02 09:45:56 -0400

    feat(macros): EmbedTest262ReportResultsTable uses encodeURIComponent & tests

--- tests/macros/EmbedTest262ReportResultsTable.test.js
+++ tests/macros/EmbedTest262ReportResultsTable.test.js
@@ -25,4 +25,11 @@ describeMacro('EmbedTest262ReportResultsTable', function() {
             ' src="https://test262.report/embed/features/dynamic-import?engines=chakra%2Cjavascriptcore%2Cspidermonkey%2Cv8&summary=true&include-browsers=true"></iframe>'
         );
     });
+    itMacro('Feature Tag: Encoded with encodeURIComponent()', function(macro) {
+        return assert.eventually.equal(
+            macro.call('?dynamic=import/foo&bar'),
+            '<iframe frameborder="0" scrolling="no" style="width: 100%; height: 300px;"' +
+            ' src="https://test262.report/embed/features/%3Fdynamic%3Dimport%2Ffoo%26bar?engines=chakra%2Cjavascriptcore%2Cspidermonkey%2Cv8&summary=true&include-browsers=true"></iframe>'
+        );
+    });
 });
