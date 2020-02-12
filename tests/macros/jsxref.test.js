108592841a721aaf1d6dfdc6f0a5439d7e2c411f
Florian Scholz <fscholz@mozilla.com>
2020-02-12 17:18:45 +0100

    Remove titles from jsxref

--- tests/macros/jsxref.test.js
+++ tests/macros/jsxref.test.js
@@ -15,16 +15,9 @@ describeMacro('jsxref', function() {
             partial_slug = 'Date',
             ref_url = js_ref_url + partial_slug,
             glob_url = js_ref_url + 'Global_Objects/' + partial_slug,
-            title =
-                'Creates a JavaScript Date instance that represents a' +
-                ' single moment in time. Date objects are based on a' +
-                ' time value that is the number of milliseconds since' +
-                ' 1 January, 1970 UTC.',
             expected =
                 '<a href="' +
                 glob_url +
-                '" title="' +
-                title +
                 '">' +
                 '<code>' +
                 name +
@@ -33,8 +26,7 @@ describeMacro('jsxref', function() {
         macro.ctx.wiki.getPage = jest.fn(async url => {
             if (url === glob_url) {
                 return {
-                    slug: js_ref_slug + partial_slug,
-                    summary: title
+                    slug: js_ref_slug + partial_slug
                 };
             } else if (url === ref_url) {
                 return {};
@@ -51,15 +43,9 @@ describeMacro('jsxref', function() {
             partial_slug = 'Array/join',
             ref_url = js_ref_url + partial_slug,
             glob_url = js_ref_url + 'Global_Objects/' + partial_slug,
-            title =
-                'The join() method joins all elements of an array (or' +
-                ' an array-like object) into a string and returns this' +
-                ' string.',
             expected =
                 '<a href="' +
                 glob_url +
-                '" title="' +
-                title +
                 '">' +
                 '<code>' +
                 name +
@@ -68,8 +54,7 @@ describeMacro('jsxref', function() {
         macro.ctx.wiki.getPage = jest.fn(async url => {
             if (url === glob_url) {
                 return {
-                    slug: js_ref_slug + partial_slug,
-                    summary: title
+                    slug: js_ref_slug + partial_slug
                 };
             } else if (url === ref_url) {
                 return {};
@@ -87,14 +72,9 @@ describeMacro('jsxref', function() {
             partial_slug = 'Statements/function',
             ref_url = js_ref_url + partial_slug,
             glob_url = js_ref_url + 'Global_Objects/' + partial_slug,
-            title =
-                'The function declaration defines a function with the' +
-                ' specified parameters.',
             expected =
                 '<a href="' +
                 glob_url +
-                '" title="' +
-                title +
                 '">' +
                 '<code>' +
                 name +
@@ -103,8 +83,7 @@ describeMacro('jsxref', function() {
         macro.ctx.wiki.getPage = jest.fn(async url => {
             if (url === glob_url) {
                 return {
-                    slug: js_ref_slug + partial_slug,
-                    summary: title
+                    slug: js_ref_slug + partial_slug
                 };
             } else if (url === ref_url) {
                 return {};
@@ -125,15 +104,9 @@ describeMacro('jsxref', function() {
             partial_slug = 'Operators/yield',
             ref_url = js_ref_url + partial_slug,
             glob_url = js_ref_url + 'Global_Objects/' + partial_slug,
-            title =
-                'The yield keyword is used to pause and resume a' +
-                ' generator function (function* or legacy generator' +
-                ' function).',
             expected =
                 '<a href="' +
                 ref_url +
-                '" title="' +
-                title +
                 '">' +
                 '<code>' +
                 name +
@@ -142,8 +115,7 @@ describeMacro('jsxref', function() {
         macro.ctx.wiki.getPage = jest.fn(async url => {
             if (url === ref_url) {
                 return {
-                    slug: js_ref_slug + partial_slug,
-                    summary: title
+                    slug: js_ref_slug + partial_slug
                 };
             } else if (url === glob_url) {
                 return {};
@@ -165,16 +137,10 @@ describeMacro('jsxref', function() {
             anchor = '#Array_iteration_and_for...in',
             ref_url = js_ref_url + partial_slug,
             glob_url = js_ref_url + 'Global_Objects/' + partial_slug,
-            title =
-                'The for...in statement iterates over the enumerable' +
-                ' properties of an object. For each distinct property,' +
-                ' statements can be executed.',
             expected =
                 '<a href="' +
                 glob_url +
                 anchor +
-                '" title="' +
-                title +
                 '">' +
                 '<code>' +
                 name +
@@ -184,7 +150,6 @@ describeMacro('jsxref', function() {
             if (url === glob_url) {
                 return {
                     slug: js_ref_slug + partial_slug,
-                    summary: title
                 };
             } else if (url === ref_url) {
                 return {};
@@ -210,18 +175,11 @@ describeMacro('jsxref', function() {
             js_ref_ru_url = '/ru/docs/' + js_ref_slug,
             ref_url = js_ref_ru_url + partial_slug,
             glob_url = js_ref_ru_url + 'Global_Objects/' + partial_slug,
-            title =
-                'Массив (Array) в JavaScript является глобальным' +
-                ' объектом, который используется в создании массивов;' +
-                ' которые представляют собой спископодобные объекты' +
-                ' высокого уровня.',
             expected =
                 '<a href="' +
                 glob_url +
                 '#' +
                 anchor +
-                '" title="' +
-                title +
                 '">' +
                 '<code>' +
                 name +
@@ -232,8 +190,7 @@ describeMacro('jsxref', function() {
         macro.ctx.wiki.getPage = jest.fn(async url => {
             if (url === glob_url) {
                 return {
-                    slug: js_ref_slug + partial_slug,
-                    summary: title
+                    slug: js_ref_slug + partial_slug
                 };
             } else if (url === ref_url) {
                 return {};
@@ -256,14 +213,9 @@ describeMacro('jsxref', function() {
                 partial_slug = 'Operators/function',
                 ref_url = js_ref_url + partial_slug,
                 glob_url = js_ref_url + 'Global_Objects/' + partial_slug,
-                title =
-                    'The function keyword can be used to define a function' +
-                    ' inside an expression.',
                 expected =
                     '<a href="' +
                     ref_url +
-                    '" title="' +
-                    title +
                     '">' +
                     name +
                     '</a>';
@@ -272,7 +224,6 @@ describeMacro('jsxref', function() {
                 if (url === ref_url) {
                     return {
                         slug: js_ref_slug + partial_slug,
-                        summary: title
                     };
                 } else if (url === glob_url) {
                     return {};
@@ -294,20 +245,9 @@ describeMacro('jsxref', function() {
             partial_slug = 'Function/name',
             ref_url = js_ref_url + partial_slug,
             glob_url = js_ref_url + 'Global_Objects/' + partial_slug,
-            title =
-                "A Function object's read-only name property indicates" +
-                " the function's name as specified when it was created," +
-                ' or "anonymous" for functions created anonymously.',
-            escaped_title =
-                "A Function object's read-only name property indicates" +
-                " the function's name as specified when it was created," +
-                ' or &quot;anonymous&quot; for functions created' +
-                ' anonymously.',
             expected =
                 '<a href="' +
                 glob_url +
-                '" title="' +
-                escaped_title +
                 '">' +
                 '<code>' +
                 name +
@@ -316,8 +256,7 @@ describeMacro('jsxref', function() {
         macro.ctx.wiki.getPage = jest.fn(async url => {
             if (url === glob_url) {
                 return {
-                    slug: js_ref_slug + partial_slug,
-                    summary: title
+                    slug: js_ref_slug + partial_slug
                 };
             } else if (url === ref_url) {
                 return {};
@@ -330,28 +269,4 @@ describeMacro('jsxref', function() {
         );
     });
 
-    itMacro('Standard title is used on missing document', function(macro) {
-        // Common on newly translated pages, such as
-        // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/replace
-        // {{jsxref("RegExp.prototype.test()")}}
-        var name = 'RegExp.prototype.test()',
-            partial_slug = 'RegExp/test',
-            glob_url = js_ref_url + 'Global_Objects/' + partial_slug,
-            standard =
-                'The documentation about this has not yet been' +
-                ' written; please consider contributing!',
-            expected =
-                '<a href="' +
-                glob_url +
-                '" title="' +
-                standard +
-                '">' +
-                '<code>' +
-                name +
-                '</code></a>';
-
-        macro.ctx.wiki.getPage = jest.fn(async url => ({}));
-
-        return assert.eventually.equal(macro.call(name), expected);
-    });
 });
